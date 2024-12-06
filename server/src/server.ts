3d43f30ec06079077634d95ff14c1e1325f01def
aabazary <85041715+aabazary@users.noreply.github.com>
2024-12-06 00:25:21 -0800

    updated dirname

--- server/src/server.ts
+++ server/src/server.ts
@@ -12,6 +12,11 @@ import { authenticateToken } from './services/auth-service.js';
 // Import the two parts of a GraphQL schema
 import { typeDefs, resolvers } from './schemas/index.js';
 import db from './config/connection.js';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 
 const PORT = process.env.PORT || 3001;
